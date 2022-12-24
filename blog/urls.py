8bece7a9cf24c00471f898919ec44a143d9f2734
Daniel Assani <devasddaniel@gmail.com>
2022-12-24 16:10:52 +0200

    add article

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path("", views.home, name='home'),
     path('article/<int:id>', views.article), 
     path("article/<int:id>/edit", views.modify_article, name='article.edit'),
-    path('article/<int:id>/update', views.update_article)
+    path('article/<int:id>/update', views.update_article), 
+    path('article/add', views.add_article, name='article.add'),
+    path('article/add/save', views.insert_article, name='article.save')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -40,6 +40,23 @@ def update_article(request, id):
         return render(request, 'error.html', {'message': 'Method not allowed'})
 
 
+def add_article(request):
+    formulaire = ArticleForm()
+    formulaire.__setattr__('class', 'form-group')
+    return render(request, 'add.html', {'form': formulaire})
+
+def insert_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            article = Article(title = cd['title'], content=cd['content'])
+            article.save()
+            return render(request, 'succes.html')
+        else:
+            return render(request, 'error.html', {'message': 'erreur de soumission du formulaire'})
+    else:
+        return render(request, 'error.html', {'message': 'Method not allowed'})
 
 
 
